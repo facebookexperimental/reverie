120fbac957982fa25535c46a269dad38a5943420
Charles Cady <charlescady@fb.com>
2022-08-19 01:37:10 -0700

    Populate mmapped page trampoline with aarch64 instructions
    Summary: This will help reverie-ptrace slowly migrate towards supporting `aarch64` as well as `x86_64`.
    
    Reviewed By: jasonwhite
    
    Differential Revision: D38787872
    
    fbshipit-source-id: e747c59da8f0c59f0b85a50888c884dbbed65824

--- reverie-ptrace/src/cp/mmap.rs
+++ reverie-ptrace/src/cp/mmap.rs
@@ -20,14 +20,31 @@ use super::consts::*;
 /// x86_64-linux-gnu-objcopy -I binary /tmp/1.bin -O elf64-x86-64 -B i386:x86-64 /tmp/1.elf
 /// then objdump -d 1.elf must match the instructions listed below.
 pub fn populate_mmap_page(pid: Pid, page_address: u64) -> nix::Result<()> {
-    /* the syscall sequences used here:
+    /* For x86_64 architecture, we use the following syscall sequences:
      * 0:   0f 05                   syscall                  // untraced syscall
      * 2:   0f 0b                   ud2
      * 4:   0f 05                   syscall                  // traced syscall
      * 6:   0f 0b                   ud2
      */
+    #[cfg(target_arch = "x86_64")]
     let mut syscall_stubs: Vec<u8> = vec![0x0f, 0x05, 0x0f, 0x0b, 0x0f, 0x05, 0x0f, 0x0b];
-    syscall_stubs.resize_with(TRAMPOLINE_SIZE, || 0xcc);
+    /* For aarch64 architecture, we use the following syscall sequences:
+     * 0:   d4 00 00 01                svc 0                  // Equivalent to the syscall instruction
+     * 2:   00 00 de ad                udf #0xdead            // Equivalent to the ud2 instruction
+     * 4:   d4 00 00 01                svc 0
+     * 6:   00 00 de ad                udf #0xdead
+     */
+    #[cfg(target_arch = "aarch64")]
+    let mut syscall_stubs: Vec<u8> = vec![
+        0xd4, 0x00, 0x00, 0x01, 0x00, 0x00, 0xde, 0xad, 0xd4, 0x00, 0x00, 0x01, 0x00, 0x00, 0xde,
+        0xad,
+    ];
+    // Fill syscall_stubs with a software interrupt (or debug breakpoint) instruction until it reaches the trampoline size
+    #[cfg(target_arch = "x86_64")]
+    const SOFTWARE_INTERUPT: u8 = 0xcc; // int3 instruction opcode for x86_64
+    #[cfg(target_arch = "aarch64")]
+    const SOFTWARE_INTERUPT: u8 = 0x00; // For aarch64, we should use BRK 1 but as it is not a single-byte instruction, we'll use a sequence of 0x00 (same as a sequence of udf #0x0 instructions)
+    syscall_stubs.resize_with(TRAMPOLINE_SIZE, || SOFTWARE_INTERUPT);
     let local_iov = &[IoVec::from_slice(syscall_stubs.as_slice())];
     let remote_iov = &[RemoteIoVec {
         base: page_address as usize,
